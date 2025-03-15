b935f3794298215d295b6aa6ac2ad7f7d48734bd
Maik Wildermann <web-developer-mw@outlook.de>
2025-03-15 15:40:03 +0100

    Start - Button hinzugefügt

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 "using strict";
 
-import {RandomColor} from "./libs./tools.js"
+import {RandomColor, AddColors, Shuffle, CheckColors, StartGame} from "./src/libs/tools.js"
 
-
+// Random - Colors
 const searchColor = RandomColor();
+const colors = AddColors(11);
+colors.push(searchColor);
+const shuffleColors = Shuffle(colors);
 
-console.log(searchColor);
+// Querries - Selection
+const hexcode = document.querySelector("#hexcode");
+const buttonsColor = document.querySelectorAll(".buttons");
+const buttonsContainer = document.querySelector(".buttons-container");
+const startButton = document.querySelector(".start");
+
+
+// Start - Game
+startButton.addEventListener("click", event => {
+ if(event.target.className === "start") {
+  startButton.disabled = true;
+  StartGame(hexcode, searchColor, buttonsColor, shuffleColors, buttonsContainer, startButton);
+ }else {
+  return;
+ }
+});
--- src/libs/tools.js
+++ src/libs/tools.js
@@ -11,16 +11,16 @@ export function RandomColor() {
   return hexCode;
 }
 
-export function AddRandomColors(amount) {
-  const addColorHex = [];
+export function AddColors(amount) {
+  const colors = [];
   for(let i = 0; i < amount; i++) {
-    addColorHex.push(RandomColor());
+    colors.push(RandomColor());
   }
 
-  return addColorHex;
+  return colors;
 }
 
-export function Shuffle(array) {
+export function Shuffle(array = []) {
   for(let i = array.length - 1; i > 0; i--) {
     const index = Math.floor(Math.random() * (i + 1));
     [array[i], array[index]] = [array[index], array[i]];
@@ -35,4 +35,19 @@ export function CheckColors(hex1, hex2) {
   }else {
     return false;
   }
+}
+
+export function StartGame(hexcode, searchColor, buttonsColor, shuffleColors, buttonsContainer) {
+  hexcode.innerText = searchColor;
+  buttonsColor.forEach((element, index) => {
+    element.style.backgroundColor = shuffleColors[index];
+    element.dataset.hex = shuffleColors[index]; 
+  })
+  buttonsContainer.addEventListener("click", event => {
+    if(event.target.className !== "buttons") {
+      return;
+    }
+    const result = CheckColors(searchColor, event.target.dataset.hex);
+    console.log(result);
+  })
 }
