954c2057f688b2c0bf45e43a2f17030e9b530382
Maik Wildermann <web-developer-mw@outlook.de>
2025-03-15 14:01:14 +0100

    Test

--- index.js
+++ index.js
@@ -1,29 +1,8 @@
 "using strict";
 
-import {GenerateRandomColor, AddRandomColor, Shuffle, checkColors} from "./src/libs/tools.js";
-let mixedArray = [];
-const hexcode = document.querySelector("#hexcode");
-const colorboxes = document.querySelectorAll(".buttons");
-const buttonsContainer = document.querySelector(".buttons-container");
-
-let searchedColor = null;
-searchedColor = GenerateRandomColor();
+import {RandomColor} from "./libs./tools.js"
 
 
-mixedArray = AddRandomColor(11);
-mixedArray.push(searchedColor);
-const shuffleArray = Shuffle(mixedArray);
+const searchColor = RandomColor();
 
-hexcode.innerText = searchedColor;
-
-colorboxes.forEach((element, index) => {
-  element.style.backgroundColor = shuffleArray[index];
-  element.dataset.hex = shuffleArray[index];
-});
-
-buttonsContainer.addEventListener("click", (event) => {
-  if(event.target.className !== "buttons") {
-    return;
-  }
-  const result =  checkColors(searchedColor, event.target.dataset.hex);
-});
+console.log(searchColor);
--- src/libs/tools.js
+++ src/libs/tools.js
@@ -1,35 +1,37 @@
-"using strict";
+"using strict"
 
-export function GenerateRandomColor() {
-  let newHexCode = "#";
-  const chars = "0123456789ABCDEF";
+export function RandomColor() {
+  let hexCode = "#";
+  const chars ="0123456789ABCDEF";
   for(let i = 0; i < 6; i++) {
-    let randomIndex = Math.floor(Math.random() * 16);
-    newHexCode += chars[randomIndex]
-  }
-  return newHexCode;
+    const index = Math.floor(Math.random() * 16);
+    hexCode += chars[index];
+  }  
+
+  return hexCode;
 }
 
-export function AddRandomColor(amount) {
-  let roundColors = [];
+export function AddRandomColors(amount) {
+  const addColorHex = [];
   for(let i = 0; i < amount; i++) {
-    roundColors.push(GenerateRandomColor());
+    addColorHex.push(RandomColor());
   }
-  
-  return roundColors;
+
+  return addColorHex;
 }
 
-export function Shuffle(colorList) {
-  for(let i = colorList.length - 1; i > 0; i--) {
-    const rndIndex = Math.floor(Math.random() * (i + 1));
-    [colorList[i], colorList[rndIndex]] = [colorList[rndIndex],colorList[i]]
+export function Shuffle(array) {
+  for(let i = array.length - 1; i > 0; i--) {
+    const index = Math.floor(Math.random() * (i + 1));
+    [array[i], array[index]] = [array[index], array[i]];
   }
-  return colorList;
+
+  return array;
 }
 
-export function checkColors(hex1, hex2) {
+export function CheckColors(hex1, hex2) {
   if(hex1 === hex2) {
-    return true
+    return true;
   }else {
     return false;
   }
